f51ba0d9452f6ad2246a2363b72235710113d6a3
root <root@lgrimmei.lgrimmei.42.fr>
2024-05-30 21:19:50 +0200

    feat: script still not working

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+echo "Database name: '$MYSQL_DB_NAME'"
+
 # Start the MySQL service
 service mysql start
 
 # Create the database (if it doesn't exist)
-mysql -uroot -p  -e "CREATE DATABASE IF NOT EXISTS TESTDB"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DB_NAME\`"
 
 # Create the user (if it doesn't exist)
-mysql -uroot -p  -e "CREATE USER IF NOT EXISTS TESTUS@'localhost' IDENTIFIED BY 'TESTPW'"
-
-# Note: Replace 'TESTPW' with your actual password
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PW'"
 
 echo "Database and user created (if not already existing)."
 
-mysql -e "GRANT ALL PRIVILEGES ON TESTDB.* TO TESTUS@'localhost' IDENTIFIED BY 'TESTPW';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'TESTPW';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DB_NAME\`.* TO '$MYSQL_USER'@'localhost'"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
 exec mysqld_safe
