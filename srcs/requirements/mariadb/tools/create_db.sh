ef86d89937f8402c1bb1193d0a824e702a5bf058
root <root@lgrimmei.lgrimmei.42.fr>
2024-07-15 19:44:39 +0200

    feat: rework Dockerfiles to bullseye

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Start MariaDB server in background
-mysqld_safe &
-
-# Wait for the MySQL server to be up and running
+# start mariadb server and wait for it to be up and running
+service mariadb start
 until mysqladmin ping &>/dev/null; do
     echo "Waiting for MariaDB to start..."
     sleep 1
 done
 
-# Check if database already exists
-DB_EXISTS=$(mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES LIKE '$MYSQL_DB_NAME'" | grep "$MYSQL_DB_NAME" > /dev/null; echo "$?")
+# check if database already exists
+DB_EXISTS=$(mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "SHOW DATABASES LIKE '$MYSQL_DB_NAME'" | grep "$MYSQL_DB_NAME" > /dev/null; echo "$?")
 
+# create database if it does not exist
 if [ "$DB_EXISTS" -eq 1 ]; then
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DB_NAME\`" || { echo 'Failed to create database'; exit 1; }
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'" || { echo 'Failed to create user'; exit 1; }
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DB_NAME\`.* TO '$MYSQL_USER'@'%'" || { echo 'Failed to grant privileges'; exit 1; }
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;" || { echo 'Failed to flush privileges'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DB_NAME\`" || { echo 'Failed to create database'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'" || { echo 'Failed to create user'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DB_NAME\`.* TO '$MYSQL_USER'@'%'" || { echo 'Failed to grant privileges'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;" || { echo 'Failed to flush privileges'; exit 1; }
 fi
 
-# Keep container running
+# shutdown and restart mariadb in background and with new config
+mysqladmin -u root -p$MDB_ROOT_PASSWORD schutdown
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+
+# create never ending process in foreground to keep container running
 tail -f /dev/null
