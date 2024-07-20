878b0c79a9114832428d409518ad0489c112fca8
root <root@lgrimmei.lgrimmei.42.fr>
2024-07-20 22:36:07 +0200

    feat: add password generation and secrets

--- srcs/init.sh
+++ srcs/init.sh
@@ -20,23 +20,49 @@ else
   echo "Directory $DIR created."
 fi
 
+# CREATE SSL DIRECTORY
+DIR="/home/lgrimmei/data/ssl"
+if [ -d "$DIR" ]; then
+  echo "Directory $DIR already exists."
+else
+  echo "Directory $DIR does not exist. Creating it now..."
+  mkdir -p "$DIR"
+  
+  echo "Directory $DIR created."
+fi
+
+# Function to generate a random password
+generate_password() {
+  openssl rand -base64 16
+}
+
+if [ -d "secrets" ]; then
+    echo "secrets folder already exists"
+else
+    mkdir secrets 
+    generate_password > secrets/wp_user_password.txt
+    generate_password > secrets/wp_root_password.txt
+    generate_password > secrets/db_password.txt
+    generate_password > secrets/db_root_password.txt
+fi
+
 # CREATE ENV VARIABLES
-if [ -f ".env" ]; then
+if [ -f "srcs/.env" ]; then
     echo ".env already exists"
 else
     echo "DOMAIN_NAME=lgrimmei.42.fr" >> srcs/.env
     echo "#mariadb" >> srcs/.env
     echo "MDB_USER=lgrimmei" >> srcs/.env
     echo "MDB_DB_NAME=database" >> srcs/.env
-    echo "MDB_PW=ilove42" >> srcs/.env
-    echo "MDB_ROOT_PASSWORD=ilove42" >> srcs/.env
+    #echo "MDB_PW=ilove42" >> srcs/.env
+    #echo "MDB_ROOT_PASSWORD=ilove42" >> srcs/.env
     echo "#wordpress" >> srcs/.env
     echo "WP_TITLE=lgrimmei" >> srcs/.env
     echo "WP_ADMIN_NAME=admin" >> srcs/.env
-    echo "WP_ADMIN_PW=ilove42" >> srcs/.env
+    #echo "WP_ADMIN_PW=ilove42" >> srcs/.env
     echo "WP_ADMIN_EMAIL=admin@gmail.com" >> srcs/.env
     echo "WP_USER_NAME=user" >> srcs/.env
-    echo "WP_USER_PW=ilove42" >> srcs/.env
+    #echo "WP_USER_PW=ilove42" >> srcs/.env
     echo "WP_USER_EMAIL=user@gmail.com" >> srcs/.env
     echo "WP_USER_ROLE=author" >> srcs/.env
     echo ".env created"
--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -12,10 +12,10 @@ DB_EXISTS=$(mysql -uroot -p"$MDB_PW" -e "SHOW DATABASES LIKE '$MDB_DB_NAME'" | g
 
 # create database if it does not exist
 if [ "$DB_EXISTS" -eq 1 ]; then
-    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MDB_DB_NAME\`" || { echo 'Failed to create database'; exit 1; }
-    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PW'" || { echo 'Failed to create user'; exit 1; }
-    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$MDB_DB_NAME\`.* TO '$MDB_USER'@'%'" || { echo 'Failed to grant privileges'; exit 1; }
-    mysql -uroot -p"$MDB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;" || { echo 'Failed to flush privileges'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PW" -e "CREATE DATABASE IF NOT EXISTS \`$MDB_DB_NAME\`" || { echo 'Failed to create database'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PW" -e "CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PW'" || { echo 'Failed to create user'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PW" -e "GRANT ALL PRIVILEGES ON \`$MDB_DB_NAME\`.* TO '$MDB_USER'@'%'" || { echo 'Failed to grant privileges'; exit 1; }
+    mysql -uroot -p"$MDB_ROOT_PW" -e "FLUSH PRIVILEGES;" || { echo 'Failed to flush privileges'; exit 1; }
 fi
 
 # stop the MariaDB service started by the service command
@@ -25,7 +25,7 @@ service mariadb stop
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 
 # shutdown and restart mariadb in background and with new config
-#mysqladmin -u root -p$MDB_ROOT_PASSWORD # shutdown
+#mysqladmin -u root -p$MDB_ROOT_PW # shutdown
 #mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 
 # create never ending process in foreground to keep container running
