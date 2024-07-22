6045849fe33dcac870dadbc98501396b53c1c663
root <root@lgrimmei.lgrimmei.42.fr>
2024-07-22 18:00:45 +0200

    feat: improve init scripts

--- srcs/init.sh
+++ srcs/init.sh
@@ -8,45 +8,47 @@ generate_password() {
 # CREATE WORDPRESS DIRECTORY
 DIR="/home/lgrimmei/data/wordpress"
 if [ -d "$DIR" ]; then
-  echo -e "\e[33m✔\e[0m Directory $DIR already exists."
-else
-  mkdir -p "$DIR"
-  echo -e "\e[32m✔\e[0m Directory $DIR created."
+  rm -fr "$DIR"
+  echo -e "\e[33m✔\e[0m Directory $DIR removed."
 fi
+mkdir -p "$DIR"
+echo -e "\e[32m✔\e[0m Directory $DIR created."
 
 # CREATE MARIADB DIRECTORY
 DIR="/home/lgrimmei/data/mariadb"
 if [ -d "$DIR" ]; then
-  echo -e "\e[33m✔\e[0m Directory $DIR already exists."
-else
-  mkdir -p "$DIR"
-  echo -e "\e[32m✔\e[0m Directory $DIR created."
+  rm -fr "$DIR"
+  echo -e "\e[33m✔\e[0m Directory $DIR removed."
 fi
+mkdir -p "$DIR"
+echo -e "\e[32m✔\e[0m Directory $DIR created."
 
 # CREATE SECRETS DIRECTORY AND GENERATE PASSWORDS
-if [ -d "secrets" ]; then
-  echo -e "\e[33m✔\e[0m Secrets folder already exists."
-else
-  mkdir secrets
-  generate_password > secrets/wp_user_password.txt
-  generate_password > secrets/wp_root_password.txt
-  generate_password > secrets/db_password.txt
-  generate_password > secrets/db_root_password.txt
-  echo -e "\e[32m✔\e[0m Secrets folder created and passwords generated."
+DIR="/home/lgrimmei/Desktop/Inception/secrets"
+if [ -d "$DIR" ]; then
+  rm -fr "$DIR"
+  echo -e "\e[33m✔\e[0m Directory $DIR removed."
+mkdir secrets
+echo -e "\e[32m✔\e[0m Directory $DIR created."
+generate_password > secrets/wp_user_password.txt
+generate_password > secrets/wp_root_password.txt
+generate_password > secrets/db_password.txt
+generate_password > secrets/db_root_password.txt
+echo -e "\e[32m✔\e[0m Secrets created."
 fi
 
 # CREATE SSL DIRECTORY
 DIR="/home/lgrimmei/data/ssl"
 if [ -d "$DIR" ]; then
-  echo -e "\e[33m✔\e[0m Directory $DIR already exists."
-else
-  mkdir -p "$DIR"
-  echo -e "\e[32m✔\e[0m Directory $DIR created."
+  rm -fr "$DIR"
+  echo -e "\e[33m✔\e[0m Directory $DIR removed."
 fi
+mkdir -p "$DIR"
+echo -e "\e[32m✔\e[0m Directory $DIR created."
 
 # CREATE SSL CERTIFICATES
 if [ ! -f "secrets/inception.crt" ] || [ ! -f "secrets/inception.key" ]; then
-  openssl req -x509 -nodes -out secrets/inception.crt -keyout secrets/inception.key -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=lgrimmei.42.fr/UID=lgrimmei" 2> /dev/null
+  openssl req -x509 -nodes -out secrets/inception.crt -keyout secrets/inception.key -subj "/C=DE/ST=IDF/L=BERLIN/O=42/OU=42/CN=lgrimmei.42.fr/UID=lgrimmei.42.fr" 2> /dev/null
   echo -e "\e[32m✔\e[0m SSL certificates created."
 else
   echo -e "\e[33m✔\e[0m SSL certificates already exist."
@@ -54,18 +56,18 @@ fi
 
 # CREATE ENV VARIABLES
 if [ -f "srcs/.env" ]; then
-  echo -e "\e[33m✔\e[0m .env file already exists."
-else
-  echo "DOMAIN_NAME=lgrimmei.42.fr" >> srcs/.env
-  echo "#mariadb" >> srcs/.env
-  echo "MDB_USER=lgrimmei" >> srcs/.env
-  echo "MDB_DB_NAME=database" >> srcs/.env
-  echo "#wordpress" >> srcs/.env
-  echo "WP_TITLE=inception" >> srcs/.env
-  echo "WP_ADMIN_NAME=lgrimmei" >> srcs/.env
-  echo "WP_ADMIN_EMAIL=lgrimmei@gmail.com" >> srcs/.env
-  echo "WP_USER_NAME=user" >> srcs/.env
-  echo "WP_USER_EMAIL=user@gmail.com" >> srcs/.env
-  echo "WP_USER_ROLE=author" >> srcs/.env
-  echo -e "\e[32m✔\e[0m .env file created."
+  rm -fr srcs/.env
+  echo -e "\e[33m✔\e[0m .env file removed."
 fi
+echo "DOMAIN_NAME=lgrimmei.42.fr" >> srcs/.env
+echo "#mariadb" >> srcs/.env
+echo "MDB_USER=lgrimmei" >> srcs/.env
+echo "MDB_DB_NAME=database" >> srcs/.env
+echo "#wordpress" >> srcs/.env
+echo "WP_TITLE=inception" >> srcs/.env
+echo "WP_ADMIN_NAME=lgrimmei" >> srcs/.env
+echo "WP_ADMIN_EMAIL=lgrimmei@gmail.com" >> srcs/.env
+echo "WP_USER_NAME=user" >> srcs/.env
+echo "WP_USER_EMAIL=user@gmail.com" >> srcs/.env
+echo "WP_USER_ROLE=author" >> srcs/.env
+echo -e "\e[32m✔\e[0m .env file created."
